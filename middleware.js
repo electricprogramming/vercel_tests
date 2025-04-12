2504a950910e9a440efc50d3552fcd5eceb32725
electricprogramming <yeungj316@gmail.com>
2025-04-12 13:04:15 -0400

    hm

--- middleware.js
+++ middleware.js
@@ -1,16 +1,18 @@
 export default async function middleware(req) {
-  const urls = [
-    '/a.html',
-    '/b.html',
-    '/c.html',
-  ]
+  if (req.url.pathname === '/') {
+    const urls = [
+      '/a.html',
+      '/b.html',
+      '/c.html',
+    ]
 
-  // Randomly choose one of the URLs to rewrite to
-  const randomUrl = urls[Math.floor(Math.random() * urls.length)]
+    // Randomly choose one of the URLs to rewrite to
+    const randomUrl = urls[Math.floor(Math.random() * urls.length)]
 
-  // Use Vercel's built-in rewrite functionality (not @vercel/edge)
-  const url = new URL(req.url)
-  url.pathname = randomUrl // Update the pathname to one of the options
+    // Use Vercel's built-in rewrite functionality (not @vercel/edge)
+    const url = new URL(req.url)
+    url.pathname = randomUrl // Update the pathname to one of the options
 
-  return Response.redirect(url, 307) // Issue a rewrite by redirecting internally
+    return Response.redirect(url, 307) // Issue a rewrite by redirecting internally
+  }
 }
